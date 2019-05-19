03cc0e1d2009dd00833047b54126a2061fbd2274
tstellanova <tstellanova@users.noreply.github.com>
2019-05-19 09:44:39 -0700

    rosrust dependencies building OK on x86_64

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
 #[macro_use]
-extern crate rosrust_codegen;
+extern crate rosrust;
 
 
 mod msg {
     rosmsg_include!(sensor_msgs/Image,sensor_msgs/Imu,geometry_msgs/TransformStamped);
+}
+
+
+pub fn instantiate_imu_msg() {
+    let mut le_msg = msg::sensor_msgs::Imu::default();
+    le_msg.linear_acceleration_covariance[0] = 0.0f64;
+    println!("orientated ");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn instantiate_expected_msgs() {
+        instantiate_imu_msg();
+	    assert!(true);
+    }
 }
 
